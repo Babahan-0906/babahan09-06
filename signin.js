73c2afc67e28bd4bba9ed687102ba7dd04e43f3c
Babahan-0906 <84456675+Babahan-0906@users.noreply.github.com>
2021-07-17 22:31:40 +0500

    Update signin.js

--- signin.js
+++ signin.js
@@ -2,6 +2,7 @@ function onSignIn(googleUser) {
 
     var profile = googleUser.getBasicProfile();
     $("#login").text(profile.getName());
+    $("#login").attr('title', 'username');
     alert('lol');
     document.getElementById('login').removeAttribute('href');
 }
