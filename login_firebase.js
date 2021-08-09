c4a4a95121b3dacf506c76d477cd0d0b8647aefc
Babahan-0906 <84456675+Babahan-0906@users.noreply.github.com>
2021-08-09 13:17:53 +0500

    Create login_firebase.js

---
+++ login_firebase.js
@@ -0,0 +1,18 @@
+ var firebaseConfig = {
+     apiKey: "AIzaSyDTV_e1m19GlKg4sc5N7B0Wv5mAYO_o2UY",
+     authDomain: "rozmebel-fa6a5.firebaseapp.com",
+     databaseURL: "https://rozmebel-fa6a5-default-rtdb.firebaseio.com",
+     projectId: "rozmebel-fa6a5",
+     storageBucket: "rozmebel-fa6a5.appspot.com",
+     messagingSenderId: "538246854850",
+     appId: "1:538246854850:web:cac696058cf10bddb8e35b",
+     measurementId: "G-5VNWXZQG04"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
+ var data = {
+     Name: "Giub",
+     Age: "Giub"
+ };
+ firebase.database().ref("Answers").set(data);
