965b94ec1f917a0f1ba4e1bc569b6e609e4965ad
Babahan-0906 <84456675+Babahan-0906@users.noreply.github.com>
2021-08-13 22:44:43 +0500

    Update login_firebase.js

--- login_firebase.js
+++ login_firebase.js
@@ -1,16 +1,23 @@
- var firebaseConfig = {
-     apiKey: "AIzaSyDTV_e1m19GlKg4sc5N7B0Wv5mAYO_o2UY",
-     authDomain: "rozmebel-fa6a5.firebaseapp.com",
-     databaseURL: "https://rozmebel-fa6a5-default-rtdb.firebaseio.com",
-     projectId: "rozmebel-fa6a5",
-     storageBucket: "rozmebel-fa6a5.appspot.com",
-     messagingSenderId: "538246854850",
-     appId: "1:538246854850:web:cac696058cf10bddb8e35b",
-     measurementId: "G-5VNWXZQG04"
- };
+var firebaseConfig = {
+    apiKey: "AIzaSyDTV_e1m19GlKg4sc5N7B0Wv5mAYO_o2UY",
+    authDomain: "rozmebel-fa6a5.firebaseapp.com",
+    databaseURL: "https://rozmebel-fa6a5-default-rtdb.firebaseio.com",
+    projectId: "rozmebel-fa6a5",
+    storageBucket: "rozmebel-fa6a5.appspot.com",
+    messagingSenderId: "538246854850",
+    appId: "1:538246854850:web:cac696058cf10bddb8e35b",
+    measurementId: "G-5VNWXZQG04"
+};
  // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
- firebase.analytics();
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.database().ref('srcnt').once('value', function(snapshot){    
+    if (snapshot.val() == null)
+    {
+        firebase.database().ref('srcnt').set(1)
+        // console.log("snapshot.val()")
+    }
+})
 //  var data = {
 //      Name: "Giub",
 //      Age: "Giub"
