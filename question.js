4dad120ca4350283e746c4092135c9ca2ce91ce4
Babahan-0906 <84456675+Babahan-0906@users.noreply.github.com>
2021-08-15 00:47:10 +0500

    Update question.js

--- question.js
+++ question.js
@@ -1,4 +1,19 @@
 function onSignIn (googleUser){
+    var profile = googleUser.getBasicProfile(), user_num;
+    firebase.database().ref('users').orderByChild('Email').equalTo(profile.getEmail()).once("value", function(snapshot) {
+        snapshot.forEach(function(data) {
+            user_num = data.key
+            console.log(data.key)
+            console.log(user_num)
+        })
+    })
+    firebase.database().ref('users/' + user_num + '/qstncnt').once('value', function(snapshot){          
+        if (snapshot.val() == null)
+        {
+            firebase.database().ref('users/' + user_num + '/qstncnt').set(1)
+        }
+    })
+    
     $('.chat_send_img').on('click', function(){
     
         var chat_letter = $('#chat_text').val(), profile = googleUser.getBasicProfile(), user_num;
